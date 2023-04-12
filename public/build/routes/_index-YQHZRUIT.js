4b45ab8f680cb24400dafeaf3a34f17fca296955
Moheuddinsheikh <101170167+Moheuddinsheikh@users.noreply.github.com>
2023-04-12 13:58:33 +0500

    Add files via upload

---
+++ public/build/routes/_index-YQHZRUIT.js
@@ -0,0 +1,85 @@
+import {
+  __toESM,
+  require_jsx_dev_runtime
+} from "/build/_shared/chunk-EETRBLDB.js";
+
+// app/routes/_index.jsx
+var import_jsx_dev_runtime = __toESM(require_jsx_dev_runtime());
+var meta = () => {
+  return [{ title: "New Remix App" }];
+};
+function Index() {
+  return /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("div", { style: { fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }, children: [
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("h1", { children: "Welcome to Remix" }, void 0, false, {
+      fileName: "app/routes/_index.jsx",
+      lineNumber: 8,
+      columnNumber: 7
+    }, this),
+    /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("ul", { children: [
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("li", { children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(
+        "a",
+        {
+          target: "_blank",
+          href: "https://remix.run/tutorials/blog",
+          rel: "noreferrer",
+          children: "15m Quickstart Blog Tutorial"
+        },
+        void 0,
+        false,
+        {
+          fileName: "app/routes/_index.jsx",
+          lineNumber: 11,
+          columnNumber: 11
+        },
+        this
+      ) }, void 0, false, {
+        fileName: "app/routes/_index.jsx",
+        lineNumber: 10,
+        columnNumber: 9
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("li", { children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)(
+        "a",
+        {
+          target: "_blank",
+          href: "https://remix.run/tutorials/jokes",
+          rel: "noreferrer",
+          children: "Deep Dive Jokes App Tutorial"
+        },
+        void 0,
+        false,
+        {
+          fileName: "app/routes/_index.jsx",
+          lineNumber: 20,
+          columnNumber: 11
+        },
+        this
+      ) }, void 0, false, {
+        fileName: "app/routes/_index.jsx",
+        lineNumber: 19,
+        columnNumber: 9
+      }, this),
+      /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("li", { children: /* @__PURE__ */ (0, import_jsx_dev_runtime.jsxDEV)("a", { target: "_blank", href: "https://remix.run/docs", rel: "noreferrer", children: "Remix Docs" }, void 0, false, {
+        fileName: "app/routes/_index.jsx",
+        lineNumber: 29,
+        columnNumber: 11
+      }, this) }, void 0, false, {
+        fileName: "app/routes/_index.jsx",
+        lineNumber: 28,
+        columnNumber: 9
+      }, this)
+    ] }, void 0, true, {
+      fileName: "app/routes/_index.jsx",
+      lineNumber: 9,
+      columnNumber: 7
+    }, this)
+  ] }, void 0, true, {
+    fileName: "app/routes/_index.jsx",
+    lineNumber: 7,
+    columnNumber: 5
+  }, this);
+}
+export {
+  Index as default,
+  meta
+};
+//# sourceMappingURL=/build/routes/_index-YQHZRUIT.js.map
